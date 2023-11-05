7016956ad8f2eea5708a7f0c698d2f6f5f1bc449
Soyeon Yang <48446896+Noeyso@users.noreply.github.com>
2023-11-05 18:19:44 +0900

    chore: emotion 설치 및 global style 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from "@emotion/react";
+import GlobalStyle from "../styles/Global";
+import theme from "../styles/theme";
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<ThemeProvider theme={theme}>
+			<div>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
---
+++ styles/Global.tsx
@@ -0,0 +1,15 @@
+// Global.tsx
+import { Global, css } from "@emotion/react";
+
+const styles = css`
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		font-size: 16px;
+	}
+`;
+
+const GlobalStyle = () => <Global styles={styles} />;
+
+export default GlobalStyle;
